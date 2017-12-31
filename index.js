806ccd0f350016ef0c90b9e7e9fa293237ea6873
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-12-31 14:08:01 +0100

    Get port from process.env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World2!'))
-
-app.listen(5000, () => console.log('listen on port 5000'))
+app.listen(PORT, () => console.log(`listen on ${ PORT }`))
