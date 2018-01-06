34b88fc77f490dce0bc3a825350f013111adb890
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2018-01-06 16:56:21 +0100

    add anguar cli template project

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 let db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 MongoClient.connect(process.env.MONGODB_URI || require('./secrets.json').connectionString, (err, client) => {
     if (err) return console.log(err);
     console.log('successfully connected to db');
@@ -15,8 +13,6 @@ MongoClient.connect(process.env.MONGODB_URI || require('./secrets.json').connect
     app.listen(PORT, () => console.log(`listen on ${PORT}`));
 });
 
-
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.post('/matches', (req, res) => {
     console.log(req.body);
     db.collection('matches').save(req.body, (err, result) => {
