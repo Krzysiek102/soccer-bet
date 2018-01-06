8d817e1451c99c78b9e8a909a248ee0199a34aa1
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2018-01-06 09:53:34 +0100

    Switch start to index.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World2!'))
-app.listen(PORT, () => console.log(`listen on ${ PORT }`))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.listen(PORT, () => console.log(`listen on ${PORT}`));
