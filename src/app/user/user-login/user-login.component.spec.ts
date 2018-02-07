95957336c6c4de2f30bc61d0752e9b65cb782358
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2018-02-07 18:01:29 +0100

    Fix unit tests

--- src/app/user/user-login/user-login.component.spec.ts
+++ src/app/user/user-login/user-login.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserLoginComponent } from './user-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UserLoginComponent', () => {
   let component: UserLoginComponent;
@@ -8,6 +11,14 @@ describe('UserLoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        HttpClientModule,
+        MatInputModule,
+      ],
       declarations: [ UserLoginComponent ]
     })
     .compileComponents();
